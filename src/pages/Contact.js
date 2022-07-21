2ccf3cc1e13e58379db2e2eeed3a841bbc1678af
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-21 16:51:01 -0400

    contact page completed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ContactOne from '../components/contact/ContactOne'
 import ContactTwo from '../components/contact/ContactTwo'
+import Footer from '../components/Footer/Footer'
+import GoogleMaps from '../components/GoogleMaps'
 
 const Contact = () => {
   return (
     <div>
       <ContactOne />
       <ContactTwo />
+      <GoogleMaps />
+      <Footer />
     </div>
   )
 }

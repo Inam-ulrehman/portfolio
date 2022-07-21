4dcb8c5df3601362be43e683c45e2f58a512795f
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-21 14:15:52 -0400

    blog page final

--- src/pages/SmallBusinessBlog.js
+++ src/pages/SmallBusinessBlog.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import BlogOne from '../components/Blog/BlogOne'
 import BlogTwo from '../components/Blog/BlogTwo'
+import Footer from '../components/Footer/Footer'
 
 const SmallBusinessBlog = () => {
   return (
     <div>
       <BlogOne />
       <BlogTwo />
+      <hr />
+      <Footer />
     </div>
   )
 }

8b102bbd3d4e993f63fd958ca4d2c51245b2b2b4
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-07-22 15:40:57 -0400

    bug

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
+import LandingFour from '../components/LandingPages/LandingFour'
 import LandingFourFullScreen from '../components/LandingPages/LandingFourFullScreen'
 
 const Services = () => {
   return (
     <div>
       <LandingFourFullScreen />
+      <LandingFour />
       <Footer />
     </div>
   )
